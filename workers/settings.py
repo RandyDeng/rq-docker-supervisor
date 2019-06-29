2d7367b38b02314cc8d1ae946ad55db46f5ce59d
Randy Deng <ryangdeng@gmail.com>
2019-06-29 00:46:46 -0400

    Initial commit with docker files

---
+++ workers/settings.py
@@ -0,0 +1,9 @@
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+
+#You can also specify the Redis DB to use
+#REDIS_DB = 3
+#REDIS_PASSWORD = 'very secret'
+
+#Queues to listen on
+QUEUES = ['high_priority', 'low_priority']
