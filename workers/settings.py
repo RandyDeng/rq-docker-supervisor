2fb4cf80de1744e8367ccde3db9b3e9bf22d686c
Randy Deng <ryangdeng@gmail.com>
2019-06-29 01:16:01 -0400

    Simplified dockerfile commands

--- workers/settings.py
+++ workers/settings.py
@@ -1,9 +1,9 @@
 REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 
-#You can also specify the Redis DB to use
-#REDIS_DB = 3
-#REDIS_PASSWORD = 'very secret'
+# You can also specify the Redis DB to use
+# REDIS_DB = 3
+# REDIS_PASSWORD = 'very secret'
 
-#Queues to listen on
+# Queues to listen on
 QUEUES = ['high_priority', 'low_priority']
